ae934e32a74316e4c180b4ac705061486a8a0f39
Joshua Tice <joshtice@gmail.com>
2019-06-25 20:40:38 -0500

    Re-format app.py using black

--- app.py
+++ app.py
@@ -27,20 +27,20 @@ app = Flask(__name__)
 
 
 # App configuration
-app.config['SECRET_KEY'] = 'supersecretkeygoeshere'
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+app.config["SECRET_KEY"] = "supersecretkeygoeshere"
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 # Dropzone configuration
 dropzone = Dropzone(app)
-app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
-app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
-app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
-app.config['DROPZONE_REDIRECT_VIEW'] = 'uploaded_images'
+app.config["DROPZONE_UPLOAD_MULTIPLE"] = True
+app.config["DROPZONE_ALLOWED_FILE_CUSTOM"] = True
+app.config["DROPZONE_ALLOWED_FILE_TYPE"] = "image/*"
+app.config["DROPZONE_REDIRECT_VIEW"] = "uploaded_images"
 
 # Uploads configuration
-app.config['UPLOADED_PHOTOS_DEST'] = Path.cwd() / 'uploads'
-photos = UploadSet('photos', IMAGES)
+app.config["UPLOADED_PHOTOS_DEST"] = Path.cwd() / "uploads"
+photos = UploadSet("photos", IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app)
 
@@ -55,7 +55,7 @@ def load_resnet50_model():
     """
 
     clear_session()
-    resnet50_model = resnet50.ResNet50(weights='imagenet')
+    resnet50_model = resnet50.ResNet50(weights="imagenet")
     global resnet50_graph
     resnet50_graph = tf.get_default_graph()
     return resnet50_model
@@ -75,16 +75,14 @@ def load_xception_model():
     clear_session()
     xception_model = Sequential()
     xception_model.add(GlobalAveragePooling2D(input_shape=(7, 7, 2048)))
-    xception_model.add(Dense(133, activation='softmax'))
+    xception_model.add(Dense(133, activation="softmax"))
     xception_model.compile(
-        loss='categorical_crossentropy',
-        optimizer='rmsprop',
-        metrics=['accuracy'])
-    xception_model.load_weights('utility_files/weights.best.Xception.hdf5')
+        loss="categorical_crossentropy", optimizer="rmsprop", metrics=["accuracy"]
+    )
+    xception_model.load_weights("utility_files/weights.best.Xception.hdf5")
     global xception_graph
     xception_graph = tf.get_default_graph()
     return xception_model
-
 
 
 def face_detector(img_path):
@@ -102,7 +100,8 @@ def face_detector(img_path):
     """
 
     face_cascade = cv2.CascadeClassifier(
-        'utility_files/haarcascade_frontalface_alt.xml')
+        "utility_files/haarcascade_frontalface_alt.xml"
+    )
     img = cv2.imread(img_path)
     grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(grayscale_img)
@@ -145,7 +144,8 @@ def dog_detector(img_path):
     img = resnet50.preprocess_input(path_to_tensor(img_path))
     with resnet50_graph.as_default():
         prediction = np.argmax(resnet50_model.predict(img))
-    return ((prediction <= 268) & (prediction >= 151))
+
+    return (prediction <= 268) & (prediction >= 151)
 
 
 def predict_breed(img_path):
@@ -162,14 +162,14 @@ def predict_breed(img_path):
         The predicted breed of the dog in the image
     """
 
-    with open('utility_files/dog_names.pickle', 'rb') as f:
+    with open("utility_files/dog_names.pickle", "rb") as f:
         dog_names = pickle.load(f)
 
     xception_model = load_xception_model()
     img = xception.preprocess_input(path_to_tensor(img_path))
     bottleneck_feature = xception.Xception(
-        weights='imagenet',
-        include_top=False).predict(img)
+        weights="imagenet", include_top=False
+    ).predict(img)
     with xception_graph.as_default():
         predicted_vector = xception_model.predict(bottleneck_feature)
     prediction = dog_names[np.argmax(predicted_vector)]
@@ -188,87 +188,69 @@ def main_algorithm(img_path):
 
     if face_detector(img_path):
         breed = predict_breed(img_path)
-        return("Looks like a human! If this were a dog, though, I would"
-               " guess a {}".format(breed))
+        return (
+            "Looks like a human! If this were a dog, though, I would"
+            " guess a {}".format(breed)
+        )
     elif dog_detector(img_path):
         breed = predict_breed(img_path)
-        return("Looks like a dog! Perhaps a {}".format(breed))
+        return "Looks like a dog! Perhaps a {}".format(breed)
     else:
-        return("Well...I have now idea what this is. Apologies. "
-               "Please try another image.")
+        return (
+            "Well...I have now idea what this is. Apologies. "
+            "Please try another image."
+        )
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
 
     [file.unlink() for file in Path("./uploads").iterdir()]
-    if 'file_urls' in session:
-        session['file_urls'] = []
+    if "file_urls" in session:
+        session["file_urls"] = []
 
     # list to hold uploaded image urls
     file_urls = []
     predictions = []
 
     # handle image upload from Dropszone
-    if request.method == 'POST':
+    if request.method == "POST":
 
-        if 'file_urls' not in session:
-            session['file_urls'] = []
-        # if 'results' not in session:
-        #     session['results'] = []
-
-        file_urls = session['file_urls']
-        # results = session['results']
-
+        if "file_urls" not in session:
+            session["file_urls"] = []
+        file_urls = session["file_urls"]
         for f in request.files:
             file = request.files.get(f)
-
-            # save the file to the uploads folder
-            filename = photos.save(
-                file,
-                name=file.filename
-            )
-
-            # append image urls
+            filename = photos.save(file, name=file.filename)
             file_urls.append(photos.url(filename))
+        session["file_urls"] = file_urls
 
-            # predict dog breed based on image
-            # img_path = str(Path.cwd() / "uploads" / file.filename)
-            # result = main_algorithm(img_path)
-            # results.append(result)
-
-        session['file_urls'] = file_urls
-        # session['results'] = results
         return "uploading..."
 
-    # return dropzone template on GET request
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/results')
+@app.route("/results")
 def uploaded_images():
 
     # redirect to home if no images to display
-    if "file_urls" not in session or session['file_urls'] == []:
-        return redirect(url_for('index'))
+    if "file_urls" not in session or session["file_urls"] == []:
+        return redirect(url_for("index"))
+    file_urls = session["file_urls"]
 
-    # set the file_urls and remove the session variable
-    file_urls = session['file_urls']
-    # session.pop('file_urls', None)
-
-    return render_template('uploaded_images.html', file_urls=file_urls)
+    return render_template("uploaded_images.html", file_urls=file_urls)
 
 
-@app.route('/predictions')
+@app.route("/predictions")
 def predictions():
 
-    file_urls = session['file_urls']
-    session.pop('file_urls', None)
-    img_paths = [str(file) for file in Path.cwd().joinpath('uploads').iterdir()]
+    file_urls = session["file_urls"]
+    session.pop("file_urls", None)
+    img_paths = [str(file) for file in Path.cwd().joinpath("uploads").iterdir()]
     predictions = []
     for img_path in img_paths:
         prediction = main_algorithm(img_path)
         predictions.append(prediction)
-    return render_template('predictions.html',
-        file_urls=file_urls,
-        predictions=predictions)
+    return render_template(
+        "predictions.html", file_urls=file_urls, predictions=predictions
+    )
